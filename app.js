bec91f8fd0d3ddcb936b97fa5d89651d694041f0
A D <demiraj.armand@gmail.com>
2019-12-11 00:21:39 -0500

    final commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var paragraphs = document.getElementsByTagName("p");
+for (var i = 0; i < paragraphs.length; i++) {
+  var paragraph = paragraphs.item(i);
+  paragraph.style.setProperty("color", "blue", null);
+}
+  
+    
+
+   
